b129fb1909326e194fbf0e2c28925fbf038dc702
Jacob Powers <powersjcb@gmail.com>
2015-02-22 16:17:52 -0800

    Created Sessions controller and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
-  root              'static_pages#home'
-  get 'help'   =>   'static_pages#help'
-  get 'about' =>   'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup' =>  'users#new'
+  root               'static_pages#home'
+  get 'help'    =>   'static_pages#help'
+  get 'about'   =>   'static_pages#about'
+  get 'contact' =>   'static_pages#contact'
+  get 'signup'  =>   'users#new'
+  get 'login'   =>   'sessions#new'
+  get 'login'   =>   'sessions#create'
+  get 'logout'  =>   'sessions#destroy'
+
   resources :users
 end
