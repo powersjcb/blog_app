551a01925a4d75c4baf002203aa9db3a9d932a59
Jacob Powers <powersjcb@gmail.com>
2015-03-11 18:02:06 -0700

    Add password reset & configure heroku sendgrid email

--- app/models/user.rb
+++ app/models/user.rb
@@ -95,6 +95,13 @@ class User < ActiveRecord::Base
   end
 
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+
+
   private
 
     def downcase_email
