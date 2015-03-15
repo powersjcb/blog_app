7b4ecc84fbcf7d57f00f90de2e2cc2d93340376a
Jacob Powers <powersjcb@gmail.com>
2015-03-14 17:08:49 -0700

    Added photo uploads, ready to integrate AWS S3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,11 +7,22 @@
 #  user_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  picture    :string
 #
 
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
