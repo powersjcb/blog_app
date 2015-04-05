9292c063a94eb914f748e50d6aed6bd6a3433079
Jacob Powers <powersjcb@gmail.com>
2015-04-05 16:10:23 -0700

    Added AJAX functionality/tests for Follow/Unfollow

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:jacob)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -20,5 +21,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
     get followers_user_path(@user)
     assert_not @user.followers.empty?
     assert_match @user.followers.count.to_s, response.body
+  end
+
+  test "should follow a user in the standard way" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference "@user.following.count", 1 do
+      xhr :post, relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+
+  test "should unfollow with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference "@user.following.count", -1 do
+      xhr :delete, relationship_path(relationship)
+    end
   end
 end
